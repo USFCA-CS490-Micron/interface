2770d296077da28c2655d9254fd621e100a68b9a
A. Blakeslee Moore <andrew@abkslm.com>
2024-10-29 11:03:39 -0700

    added listening/audio processing functions

--- Interface.py
+++ Interface.py
@@ -97,3 +97,25 @@ class Interface:
         await self.wipe_display()
         return audio_arr
 
+    @staticmethod
+    def preprocess_audio(audio_arr, sample_rate, target_sr=16000):
+        if sample_rate != target_sr:
+            audio_arr = librosa.resample(audio_arr.astype(float), orig_sr=sample_rate, target_sr=target_sr)
+        audio_arr = audio_arr / np.max(np.abs(audio_arr))
+        return audio_arr.astype(np.float32)
+
+    async def analyze(self, audio_arr: np.ndarray):
+        loading_task = asyncio.create_task(self.write_loading())
+
+        try:
+            audio_arr = self.preprocess_audio(audio_arr, self.sample_rate)
+            transcription = self.whisper.transcribe(audio_arr)
+            print(f"Transcription: {transcription["text"]}")
+        finally:
+            loading_task.cancel()
+            with suppress(asyncio.CancelledError):
+                await loading_task
+
+
+
+
